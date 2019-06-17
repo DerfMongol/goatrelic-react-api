3b9f81367261e1d938446a3b9a7281c123cf5fd5
Lee Martin <leefredmart@gmail.com>
2019-06-16 21:29:18 -0400

    Made profile.js more DRY

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,7 @@ const User = require('../../models/User')
 const Fans = require('../../models/Fans')
 
 const authCheck = (req, res, next) => {
-    if(!req.user){
+    if (!req.user) {
         // if user is not logged in
         res.send(null)
     } else {
@@ -12,86 +12,38 @@ const authCheck = (req, res, next) => {
     }
 }
 
-router.get('/', authCheck, (req,res) => {
+router.get('/', authCheck, (req, res) => {
     res.send(req.user)
 })
 
 router.post('/', (req, res) => {
-    // console.log(req.user.googleId)
-    User.findOne({ googleId: req.user.googleId }).then((currentUser) => {
-        
-        currentUser.nba = req.body.nba
 
-        if (req.body.nba.length > 0) {
-            Fans.Nba.findOne({googleId: req.user.googleId}).then((fanUser) => {
-                if (fanUser) {
-                    fanUser.players = req.body.nba
-                    fanUser.save()
-                } else {
-                    const newFan = new Fans.Nba({
-                        name: currentUser.username,
-                        googleId: req.user.googleId,
-                        pic: currentUser.thumbnail,
-                        date: new Date().toLocaleDateString(),
-                        players: req.body.nba
-                    })
-                    newFan.save()
-                }
-                
-            })
-            
-        }
-        
-        currentUser.nhl = req.body.nhl
-        
-        if (req.body.nhl.length > 0) {
-            
-            Fans.Nhl.findOne({googleId: req.user.googleId}).then((fanUser) => {
-                if (fanUser) {
-                    fanUser.players = req.body.nhl
-                    fanUser.save()
-                } else {
-                    const newFan = new Fans.Nhl({
-                        name: currentUser.username,
-                        googleId: req.user.googleId,
-                        pic: currentUser.thumbnail,
-                        date: new Date().toLocaleDateString(),
-                        players: req.body.nhl
-                    })
-                    newFan.save()
-                }
-                
-            })
-            
-        }
+    User.findOne({ googleId: req.user.googleId }).then(currentUser => {
 
-        currentUser.pga = req.body.pga
-        
-        if (req.body.pga.length > 0) {
-            
-            Fans.Pga.findOne({googleId: req.user.googleId}).then((fanUser) => {
+        Object.keys(Fans).forEach((sports) => {
+            Fans[sports].findOne({ googleId: req.user.googleId }).then(fanUser => {
                 if (fanUser) {
-                    fanUser.players = req.body.pga
-                    fanUser.save()
+                    fanUser.players = req.body[sports.toLocaleLowerCase()]
+                    
                 } else {
-                    const newFan = new Fans.Pga({
+                    const newFan = new Fans[sports]({
                         name: currentUser.username,
                         googleId: req.user.googleId,
                         pic: currentUser.thumbnail,
                         date: new Date().toLocaleDateString(),
-                        players: req.body.pga
+                        players: req.body[sports.toLocaleLowerCase()]
                     })
                     newFan.save()
                 }
-                
+                fanUser.save()
+
             })
-            
-        }
+            currentUser[sports.toLocaleLowerCase()] = req.body[sports.toLocaleLowerCase()]
+        })
         
         currentUser.save().then(currentUser => res.json(currentUser))
     })
+
 })
-
-
 
 module.exports = router
