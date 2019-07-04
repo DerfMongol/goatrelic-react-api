fe0e20d139e44b3d987fa2192c54bbb5267db1c8
Lee Martin <leefredmart@gmail.com>
2019-07-04 15:34:38 -0400

    heroku

--- config/keys.js
+++ config/keys.js
@@ -1,17 +1,30 @@
-module.exports = {
-    google: {
-        clientID: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET
-    },
-    mongodb: {
-        criticsURI: process.env.CRITICS_URI,
-        usersURI: process.env.USERS_URI,
-        fansURI: process.env.FANS_URI,
-        playersURI: process.env.PLAYERS_URI
-    },
-    session: {
-        cookieKey: process.env.COOKIE_KEY
-    }
-}
+const Secret = require('./secret')
 
-console.log('test')
+let config = {}
+
+if (process.env.NODE_ENV == 'development') {
+    config = Secret
+} else if (process.env.NODE_ENV == 'production') {
+    config = {
+        google: {
+            clientID: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        },
+        mongodb: {
+            criticsURI: process.env.CRITICS_URI,
+            usersURI: process.env.USERS_URI,       
+            fansURI: process.env.FANS_URI,     
+            playersURI: process.env.PLAYERS_URI
+        },
+        session: {
+            cookieKey: process.env.COOKIE_KEY
+        }
+    }
+
+}
+console.log(config)
+
+module.exports = config
+
+   
+
