1b888edb9f8dab8cb0789bcddde42a0abbbfffc9
Lee Martin <leefredmart@gmail.com>
2019-07-03 23:49:58 -0400

    heroku

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -26,7 +26,7 @@ router.get(
         if (req.get('Referrer')) {
             res.redirect('back')
         } else {
-            res.redirect('https://goatrelic.herokuapp.com')
+            res.redirect('http://goatrelic.herokuapp.com')
         }
         
     })
