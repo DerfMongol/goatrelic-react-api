3987b725fe4f5d65b696dc2112a5f36b764c39b1
Lee Martin <leefredmart@gmail.com>
2019-07-03 21:27:55 -0400

    heroku

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors());
 
-const port = 3001
+const port = process.env.PORT || 3001
 
 app.use('/auth', auth)
 app.use('/critics', critic)
