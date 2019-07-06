1d92246fe4989c79e169070755f7d2b633e5f6d5
Lee Martin <leefredmart@gmail.com>
2019-07-06 15:31:15 -0400

    heroku

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const User = require('../../models/User')
 const Fans = require('../../models/Fans')
 
 const authCheck = (req, res, next) => {
+    console.log(`auth: ${req.user}`)
     if (!req.user) {
         // if user is not logged in
         res.send(null)
